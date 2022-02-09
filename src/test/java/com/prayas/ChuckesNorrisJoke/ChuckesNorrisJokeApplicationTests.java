496f21f2999d89c3227ab0e27176214407fca4b0
Prince Kumar Singh <sprincek@cadence.com>
2022-02-09 16:05:31 +0530

    my version

---
+++ src/test/java/com/prayas/ChuckesNorrisJoke/ChuckesNorrisJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prayas.ChuckesNorrisJoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckesNorrisJokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
